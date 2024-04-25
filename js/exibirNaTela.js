6e18b2544e83082b5378ca17e6ca282558256f48
F Santos <borsatole@gmailcom>
2024-04-25 10:33:21 -0300

    alterações

--- js/exibirNaTela.js
+++ js/exibirNaTela.js
@@ -1,18 +1,18 @@
 var main = document.querySelector("#cards");
-var whatsapp = "14"
+var whatsapp = "14998816715"
+
 main.innerHTML = "";
 
 function exibirasRecargas(recargas) {
     main.innerHTML = "";
-    main.innerHTML = "";
       recargas.forEach(recarga => {
         main.innerHTML += `
         <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <img class="p-8 rounded-t-lg" src="${recarga.imagem}" alt="product image" />
+        <a ${`href="https://api.whatsapp.com/send?phone=55${whatsapp}&text=Olá, gostaria da ${recarga.titulo}"`} target="_blank">
+            <img class="p-8 rounded-t-lg" src="${recarga.imagem}" alt=${`"card da ${recarga.titulo}"`} />
         </a>
         <div class="px-5 pb-5">
-            <a href="#">
+            <a ${`href="https://api.whatsapp.com/send?phone=55${whatsapp}&text=Olá, gostaria da ${recarga.titulo}"`} target="_blank">
                 <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${recarga.titulo}</h5>
             </a>
             <div class="flex items-center mt-2.5 mb-5">
@@ -39,7 +39,7 @@ function exibirasRecargas(recargas) {
             </div>
             <div class="flex items-center justify-between">
                 <span class="text-3xl font-bold text-gray-900 dark:text-white">${recarga.preco}</span>
-                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-red-800">comprar</a>
+                <a ${`href="https://api.whatsapp.com/send?phone=55${whatsapp}&text=Olá, gostaria da ${recarga.titulo}"`} target="_blank" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-red-800">comprar</a>
             </div>
         </div></div>
           `
